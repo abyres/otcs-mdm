7f890ab22ec7614912aa6330f7562fb69de61dcd
Onn Khairuddin bin Rahmat <onn.khairuddin@gmail.com>
2018-03-11 19:00:18 +0800

    Initial import Master data management.

---
+++ src/main/java/net/abyres/tm/otcsmdm/OtcsMdmApplication.java
@@ -0,0 +1,12 @@
+package net.abyres.tm.otcsmdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtcsMdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtcsMdmApplication.class, args);
+	}
+}
---
+++ src/test/java/net/abyres/tm/otcsmdm/OtcsMdmApplicationTests.java
@@ -0,0 +1,16 @@
+package net.abyres.tm.otcsmdm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OtcsMdmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
